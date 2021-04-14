7a311bfd7521703b0b90aeaabbd1e3de441c8ff6
rk110047 <rk110047@gmail.com>
2021-04-14 13:43:08 +0530

    april14

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import  MainNavigator  from './src/navigation';
+import Loginscreen from './src/components/Loginscreen'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <MainNavigator />
     </View>
   );
 }
@@ -15,7 +16,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
